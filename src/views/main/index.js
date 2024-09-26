f29e2f3b4b5fcface5d0523f531207fce536ebb0
Boris <bobabonanadz@mail.ru>
2024-09-26 18:42:01 +0300

    Aded search component

--- src/views/main/index.js
+++ src/views/main/index.js
@@ -2,6 +2,7 @@
 import { AbstractView } from '../../common/view'
 import onChange from 'on-change' // Библиотека за слежением изменений в обьекте
 import { Header } from '../../components/header'
+import { Search } from '../../components/search'
 
 // Определение класса MainView, который наследуется от AbstractView
 export class MainView extends AbstractView {
@@ -35,8 +36,12 @@ export class MainView extends AbstractView {
     // Очистка содержимого элемента app (предполагается, что он определен в родительском классе)
     this.app.innerHTML = ''
 
+    // Добавление элемента поиск в main
+    main.append(new Search(this.state).render())
+
     // Добавление элемента main в элемент app
     this.app.append(main)
+
     this.renderHeader()
   }
 
